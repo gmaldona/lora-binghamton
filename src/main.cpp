7e631a21f6ae321e9b85dea8d99936265a3e2729
gmaldona <gmaldona@oswego.edu>
2024-11-15 22:58:43 -0500

    add file comment template

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/*
+ * CS 526 Internet of Things
+ * 
+ * Analysis of LoRa/LoRaWAN Under Varied Environmental Conditions 
+ * within the Southern Tier Region of New York State
+ *
+ * contributors: Annie Wu, Callisto Hess, Gregory Maldonado
+ * date: 2024-07-04
+ *
+ * Thomas J. Watson College of Engineering and Applied Sciences, Binghamton University
+ */
+
 #include <Arduino.h>
 
 #include "LoRaWan_APP.h"
